2ffffb237761c0a6dda536f0b9c98acdb4f54f9b
Rajan Shrestha <rajan.shr09@gmail.com>
2024-03-05 10:06:08 -0700

    path correction

--- product/category/category.js
+++ product/category/category.js
@@ -3,19 +3,19 @@
             {   
                 "id": 1,
                 "category":"Men's Clothing",
-                "image_url" : "images/category/men.png"
+                "image_url" : "/images/category/men.png"
 
             },
             {
                 "id": 2,
                 "category":"Women's Clothing",
-                "image_url" : "images/category/women.png"
+                "image_url" : "/images/category/women.png"
 
             },
             {
                 "id": 3,
                 "category":"Unisex's Clothing",
-                "image_url" : "images/category/unisex.png"
+                "image_url" : "/images/category/unisex.png"
 
             },
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,7 @@ function Home() {
   return (
     <div className="h-full mb-24 ">
       <div className=" opacity-75 sm:grid sm:grid-cols-1 sm:grid-row-1 lg:grid lg:grid-cols-3 lg:grid-rows-2">
-        <div className="row-span-2 sm:hidden lg:flex flex-col justify-center items-center lg:col-span-2 bg-[url('images/home1.jpeg')] sm:h-[400px] lg:h-[600px] bg-cover bg-no-repeat ">
+        <div className="row-span-2 sm:hidden lg:flex flex-col justify-center items-center lg:col-span-2 bg-[url('/images/home1.jpeg')] sm:h-[400px] lg:h-[600px] bg-cover bg-no-repeat ">
           <h2 className="text-4xl text-white text-center pb-4">
             Unveil Endless Choices, Shop Now
           </h2>
@@ -28,7 +28,7 @@ function Home() {
             Shop
           </Button>
         </div>
-        <div className="sm:col-span-3 sm:opacity-100 lg:col-span-1 sm:flex flex-col justify-center items-center bg-[url('images/home2.jpg')] bg-cover bg-no-repeat sm:h-[400px] lg:h-[300px]">
+        <div className="sm:col-span-3 sm:opacity-100 lg:col-span-1 sm:flex flex-col justify-center items-center bg-[url('/images/home2.jpg')] bg-cover bg-no-repeat sm:h-[400px] lg:h-[300px]">
           <h2 className="lg:hidden text-4xl bg-white m-2 opacity-70 text-center text-amber-900 pb-4">
             Unveil Endless Choices, Shop Now
           </h2>
@@ -51,18 +51,18 @@ function Home() {
       </div>
       <div className="flex sm:flex-row w-full sm:gap-x-4 md:gap-x-8 justify-center items-center sm:h-[500px] md:h-[700px]">
         <div className="flex sm:flex-col gap-y-3">
-          <div className="bg-[url('images/home4.jpg')] sm:h-[200px] sm:w-[200px] md:h-[300px] md:w-[300px] bg-cover bg-no-repeat flex justify-center items-center">
+          <div className="bg-[url('/images/home4.jpg')] sm:h-[200px] sm:w-[200px] md:h-[300px] md:w-[300px] bg-cover bg-no-repeat flex justify-center items-center">
             <p className="bg-black text-white sm:text-2xl md:text-4xl font-bold p-1">
               50% OFF
             </p>
           </div>
-          <div className="bg-[url('images/home5.jpg')] sm:h-[200px] sm:w-[200px] md:h-[300px] md:w-[300px] bg-cover bg-no-repeat flex justify-center items-center">
+          <div className="bg-[url('/images/home5.jpg')] sm:h-[200px] sm:w-[200px] md:h-[300px] md:w-[300px] bg-cover bg-no-repeat flex justify-center items-center">
             <p className="bg-black text-white sm:text-2xl md:text-4xl font-bold p-1">
               Happy Me
             </p>
           </div>
         </div>
-        <div className="bg-[url('images/home6.jpg')] sm:h-[400px] sm:w-[400px] md:h-[600px] md:w-[400px] lg:w-[600px] bg-cover bg-no-repeat flex justify-center items-center">
+        <div className="bg-[url('/images/home6.jpg')] sm:h-[400px] sm:w-[400px] md:h-[600px] md:w-[400px] lg:w-[600px] bg-cover bg-no-repeat flex justify-center items-center">
           <p className="bg-black text-white sm:text-2xl md:text-4xl font-bold p-1">
             Classic Looks
           </p>
